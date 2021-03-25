a8ef072283675f95b13942b922c56095806beb99
Jedediah Wood <jedediahwood28@gmail.com>
2021-03-24 19:46:24 -0600

    ProtectedRoute Doesnt work, otherwise OK

--- client/src/App.js
+++ client/src/App.js
@@ -8,19 +8,20 @@ import NoMatch from './components/NoMatch';
 import Login from './components/Login';
 import Register from './components/Register';
 import FetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
     <Fragment>
     <NavBar />
       <FetchUser>
-      <Container>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/login' component={ Login } />
-          <Route exact path='/register' component={Register}/>
-          <Route component={NoMatch}/>
-        </Switch>
-      </Container>
+        <Container>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/login' component={ Login } />
+            <Route exact path='/register' component={Register}/>
+            <Route component={NoMatch}/>
+          </Switch>
+        </Container>
       </FetchUser>
     </Fragment>
 )
---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React, { Component, useContext } from 'react'
+import { Redirect, Route } from 'react-router' // <--need to be "-dom"?
+import { AuthConsumer, AuthContext } from '../providers/AuthProvider'
+
+const ProtectedRoute = ({ component: Component, ...rest}) => {
+  const { authenticated } = useContext(AuthContext);
+  return (
+    <Route 
+      {...rest}
+      render={(props) => 
+        authenticated ? (<Component {...props} />) : (<Redirect to={{ pathname: "/login", 
+        state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+export default ProtectedRoute;
